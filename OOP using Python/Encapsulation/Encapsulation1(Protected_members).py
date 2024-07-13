123a466b20245906af02f620f996829982cf55a4
Tasin Mahmud <tasinmahmud44@gmail.com>
2024-07-13 13:20:06 +0600

    Add files via upload

---
+++ OOP using Python/Encapsulation/Encapsulation1(Protected_members).py
@@ -0,0 +1,53 @@
+'''
+Encapsulation describes the idea of wrapping data and the methods that work on data within one unit. 
+This puts restrictions on accessing variables and methods directly 
+and can prevent the accidental modification of data. To prevent accidental change, 
+an object’s variable can only be changed by an object’s method. 
+Those types of variables are known as private variables.
+
+Protected Members:
+Protected members are those that are intended to be accessed only within the class 
+and its subclasses.In Python, protected members are defined by prefixing the member 
+name with a single underscore (_).
+
+'''
+#In Python,encapsulation is achieved using private and protected members.
+
+#EXAMPLE-1
+class Base(object):
+    def __init__(self):
+        self._a=2
+class derived(Base):
+    def __init__(self):
+       Base.__init__(self)
+       print("Calling protected member of base class: ",self._a)
+       self._a+=3
+       print("Calling protected member of child class: ",self._a)
+
+obj1=derived()
+
+
+#EXAMPLE-2
+class BaseClass:
+    def __init__(self):
+        self._protected_attribute = 42  # Protected attribute
+
+    def _protected_method(self):
+        print("This is a protected method.")
+
+class DerivedClass(BaseClass):
+    def __init__(self):
+        super().__init__()
+        print(self._protected_attribute)  # Accessing protected attribute
+        self._protected_method()  # Accessing protected method
+
+obj = DerivedClass()
+print(obj._protected_attribute)  # This will work, but it's not recommended
+obj._protected_method()  # This will work, but it's not recommended
+
+'''
+In the above example:
+# _protected_attribute and _protected_method are protected members.
+# Protected members can be accessed within the class and its subclasses.
+# Accessing protected members outside the class or its subclasses is technically allowed but is discouraged.
+'''
---
+++ OOP using Python/Encapsulation/Encapsulation2(Private_Member).py
@@ -0,0 +1,32 @@
+'''
+Private Members
+Private members are those that should not be accessed directly outside the class. 
+In Python, private members are defined by prefixing the member name with two underscores (__).
+nor by any base class. In Python, there is no existence of Private instance 
+variables that cannot be accessed except inside a class.
+'''
+class MyClass:
+    def __init__(self):
+        self.__private_attribute = 42  # Private attribute
+        self.public_attribute = "Hello"  # Public attribute
+
+    def __private_method(self):
+        print("This is a private method.")
+
+    def public_method(self):
+        print("This is a public method.")
+        self.__private_method()  # Accessing private method within the class
+
+obj = MyClass()
+print(obj.public_attribute)  # This will work
+# print(obj.__private_attribute)  # This will raise an AttributeError
+
+obj.public_method()  # This will work
+# obj.__private_method()  # This will raise an AttributeError
+
+# Name mangling,it allows access, but it's not recommended because it breaks encapsulation.
+print(obj._MyClass__private_attribute)
+obj._MyClass__private_method()
+'''
+_classname__, where classname is the current class name with a leading underscore
+'''
