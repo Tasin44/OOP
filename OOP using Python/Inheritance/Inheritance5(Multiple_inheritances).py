6926eaa0b6f0436fbc959000346b70452d915cdf
Tasin Mahmud <tasinmahmud44@gmail.com>
2025-01-24 22:38:48 +0600

    Inheritance5(Multiple_inheritances).py

--- OOP using Python/Inheritance/Inheritance5(Multiple_inheritances).py
+++ OOP using Python/Inheritance/Inheritance5(Multiple_inheritances).py
@@ -5,30 +5,91 @@ it is called multiple inheritances.
 In multiple inheritances, all the features of the base classes are inherited into the derived class. 
 '''
 
-class Base1(object):
-	def __init__(self):
-		self.str1 = "Geek1"
-		print("Base1")
+
+class Father:
+    def __init__(self,fname):
+        self.fname=fname
+
+class Mother:
+    def __init__(self,mname):
+        self.mname=mname
+    
+class Son(Father,Mother):
+    def __init__(self, fname,mname):
+        super().__init__(fname) #We can also do :  Father.__init__(self, fname)
+        Mother.__init__(self,mname)
+    def display_parent_info(self):
+        print("Father name: ", self.fname)
+        print("Mother name: ",self.mname)
+
+obj=Son("Nannu","Taslima")
+obj.display_parent_info()
 
 
-class Base2(object):
-	def __init__(self):
-		self.str2 = "Geek2"
-		print("Base2")
+
+'''
+We cann't use super() for all parent classes;
+he super() mechanism works based on the Method Resolution Order (MRO), which means it only calls the next class in the inheritance chain as defined by the MRO.
+Therefore, you cannot directly use super() to call both Father and Mother's __init__ methods
+like super().__init__(fname)  
+     super().__init__(mname)
+
+According to the above code, the MRO is:
+
+[Son, Father, Mother, object]
+
+So, if you call super().__init__() in Son, it will invoke Father's __init__ method, but it won't automatically call Mother's __init__ method.
+'''
+
+'''
+But if we want to use super for both Father and Mother class:
+There is a solution called 
+Cooperative Initialization
+'''
+
+class Father:
+    def __init__(self,fname,**kwargs):
+        super().__init__(**kwargs)
+        self.fname=fname
+
+class Mother:
+    def __init__(self,mname,**kwargs):
+        super().__init__(**kwargs)
+        self.mname=mname
+    
+class Son(Father,Mother):
+    def __init__(self, fname,mname):
+        super().__init__(fname=fname, mname=mname)  # Pass arguments for both parents
+        
+    def display_parent_info(self):
+        print("Father name: ", self.fname)
+        print("Mother name: ",self.mname)
+
+obj=Son("Nannu","Taslima")
+obj.display_parent_info()
 
 
-class Derived(Base1, Base2):
-	def __init__(self):
-
-		# Calling constructors of Base1
-		# and Base2 classes
-		Base1.__init__(self)
-		Base2.__init__(self)
-		print("Derived")
-
-	def printStrs(self):
-		print(self.str1, self.str2)
 
 
-ob = Derived()
-ob.printStrs()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
