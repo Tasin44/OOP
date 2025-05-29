91499e19d91c869d408e08fc1d03de7124deff00
Tasin Mahmud <tasinmahmud44@gmail.com>
2025-05-29 22:53:03 +0600

    magic_methods__lt__le__eq__ne__gt__ge.py

--- OOP using Python/Class/Basic/magic_methods__lt__le__eq__ne__gt__ge.py
+++ OOP using Python/Class/Basic/magic_methods__lt__le__eq__ne__gt__ge.py
@@ -1,3 +1,13 @@
+'''
+Why _lt, ___le__ this type of magic method is need? Can't i use if else inside the cls to do the same work?
+    Enables comparison operations (<, <=) between objects.
+    Without them, if obj1 < obj2 would raise TypeError.
+    
+'''
+
+
+#These are magic methods that define comparison behavior.
+
 class Point:
     def __init__(self,x,y):
         self.x=x
@@ -50,4 +60,55 @@ print("point2 > point1", point1.greater(point2))
 print("point2 > point1 and point1>point3 and point2<point3:", point1.greater(point2),point1.greater(point3),point3.greater(point2))
         
 print("point2 > point1 and point1>point3 and point2<point3:", point2.greater(point1),point2.greater(point3),point2.greater(point1))
-        
+
+
+'''
+✅ Why not use if-else/Why Magic Methods uses instead of if/else??
+
+    No, because Python expects special methods for operators.
+
+    "if obj1 < obj2" automatically calls obj1.__lt__(obj2).
+
+    Without __lt__, Python wouldn’t know how to compare them.
+
+'''
+#Example:
+
+class Box:
+    def __init__(self, weight):
+        self.weight = weight
+
+    def __lt__(self, other):  # define < between boxes
+        return self.weight < other.weight
+
+box1 = Box(10)
+box2 = Box(20)
+
+print(box1 < box2)  # True, because 10 < 20
+
+'''
+Without __lt__, Python will throw an error:
+TypeError: '<' not supported between instances of 'Box' and 'Box'
+'''
+#like if we do(another example):
+
+class Person:
+    def __init__(self, age):
+        self.age = age
+
+p1 = Person(25)
+p2 = Person(30)
+
+if p1 < p2:  # TypeError: '<' not supported
+    print("p1 is younger")
+'''
+TypeError: '<' not supported between instances of 'Person' and 'Person'
+'''
+
+
+
+
+
+
+
+
